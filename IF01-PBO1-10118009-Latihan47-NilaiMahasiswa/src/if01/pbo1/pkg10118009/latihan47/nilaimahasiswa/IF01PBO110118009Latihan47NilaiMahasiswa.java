2376e4ad63ad5096608fefed7c1a21c52f4a13bf
TassyakurPasya <55564127+TassyakurPasya@users.noreply.github.com>
2019-11-02 15:56:15 +0700

    Add files via upload

---
+++ IF01-PBO1-10118009-Latihan47-NilaiMahasiswa/src/if01/pbo1/pkg10118009/latihan47/nilaimahasiswa/IF01PBO110118009Latihan47NilaiMahasiswa.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if01.pbo1.pkg10118009.latihan47.nilaimahasiswa;
+
+/**
+ * Nama = Tassyakur Pasya
+ * Kelas = IF01
+ * NIM = 10118009
+ * Deskripsi Program = Nilai Mahasiswa
+ * @author LENOVO
+ */
+public class IF01PBO110118009Latihan47NilaiMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        nilaiMhs mhs1 = new nilaiMhs(60, 80, 75);
+        nilaiMhs mhs2 = new nilaiMhs(75, 45, 34);
+        nilaiMhs mhs3 = new nilaiMhs(30, 80, 40);
+        
+        
+        }
+    
+}
